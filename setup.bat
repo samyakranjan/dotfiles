0257213055b136d57b7dc97b2e9b27b07cd54d36
Samyak Ranjan <masha.ranjan@gmail.com>
2025-01-06 09:54:09 +0530

    Completing setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,8 @@
 @echo off
 
-md .vim
-git clone https://github.com/VundleVim/Vundle.vim.git c:\code\sr\dotfiles\.vim\bundle\Vundle.vim
+REM IF NOT EXIST .vim MD .vim
+REM git clone https://github.com/VundleVim/Vundle.vim.git %cd%\.vim\bundle\Vundle.vim
+
+powershell -Command "(Get-Content .vimrc) -replace '@@GIT_DIR@@', (Get-Location) | Set-Content .vimrc"
+
+REM echo source %cd%\.vimrc > %USERPROFILE%\_vimrc
